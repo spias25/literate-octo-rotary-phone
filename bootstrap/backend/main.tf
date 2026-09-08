4694f96591a183a93d668444294331e66dc77eac
labuser <labuser@ip-172-31-27-185.ap-south-1.compute.internal>
2026-09-08 09:58:04 +0000

    s3 before create

--- bootstrap/backend/main.tf
+++ bootstrap/backend/main.tf
@@ -1,13 +1,20 @@
-provider "aws" { region = var.region }
+provider "aws" {
+  region = var.region
+}
 
-resource "aws_s3_bucket" "state""{
-  bucket = var.state_bucket_name
+resource "aws_s3_bucket" "state" {
+  bucket        = var.state_bucket_name
   force_destroy = false
-  tags =  { Project = var.project, Purpose = "Terraform-state" }
+
+  tags = {
+    Project = var.project
+    Purpose = "Terraform-state"
+  }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
   bucket = aws_s3_bucket.state.id
+
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -15,14 +22,15 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
   }
 }
 
-resouresource "aws_s3_bucket_public_access_block" "state" { 
-    bucket = aws_s3_bucket.state.id
-    block_public_acls = true
-    block_public_policy = true
-    ignore_public_acls = true
-    restrict_public_buckets = true
+resource "aws_s3_bucket_public_access_block" "state" {
+  bucket = aws_s3_bucket.state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-ouput "state_bucket" {
+output "state_bucket" {
   value = aws_s3_bucket.state.bucket.id
 }
--- bootstrap/backend/version.tf
+++ bootstrap/backend/version.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">=1.8.0"
   required_providers {
-    aws = { source = "hashicrop/aws, version = "~> 6.0" }
+    aws = { source = "hashicorp/aws", version = "~> 6.0" }
   }
 }
