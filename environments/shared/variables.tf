a2d5dcd00dfd9e02c40215bfb610363978015418
labuser <labuser@ip-172-31-27-185.ap-south-1.compute.internal>
2026-09-08 11:01:21 +0000

    Remove terraform binaries and update gitignore

---
+++ environments/shared/variables.tf
@@ -0,0 +1,16 @@
+variable "region" { type = string }
+variable "project" { type = string }
+variable "environment" { type = string }
+variable "cluster_name" { type = string }
+variable "kubernetes_version" { type = string }
+variable "vpc_cidr" { type = string }
+variable "azs" { type = list(string) }
+variable "private_subnet_cidrs" { type = list(string) }
+variable "public_subnet_cidrs" { type = list(string) }
+variable "instance_type" { type = list(string) }
+variable "node_min" { type = number }
+variable "node_desired" { type = number }
+variable "node_max" { type = number }
+variable "github_org" { type = string }
+variable "github_repo" { type = string }
+  
