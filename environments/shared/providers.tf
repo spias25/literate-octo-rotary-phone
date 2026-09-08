83f4cdb9555163c8dde52e4630ab45b66931a488
labuser <labuser@ip-172-31-27-185.ap-south-1.compute.internal>
2026-09-08 11:47:40 +0000

    updated env shared

---
+++ environments/shared/providers.tf
@@ -0,0 +1,16 @@
+provider "aws" { region = var.region }
+data "aws.eks_cluster_auth" "this" { name = module.eks.cluster_name }
+provider "kubernetes" { 
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws.eks_cluster_auth.this.token
+}
+provider "helm" {
+  kubernetes = {
+    host = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token = data.aws.eks_cluster_auth.this.token
+  }
+}
+
+  
