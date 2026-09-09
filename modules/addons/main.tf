aa663305b529feb18532f451806b6155269c07eb
labuser <labuser@ip-172-31-10-243.ap-south-1.compute.internal>
2026-09-09 10:36:56 +0000

    updated addons

--- modules/addons/main.tf
+++ modules/addons/main.tf
@@ -14,12 +14,39 @@ resource "helm_release" "alb" {
     }
     ]
 }
-resource "helm_release" "matrics_server" {
+resource "helm_release" "metrics_server" {
   name = "metrics-server"
   repository = "https://kubernetes.github.io/metrics-server"
     chart = "metrics-server"
     namespace = "kube-system"
 }
 resource "helm_release" "cluster_autoscaler" {
-  
+  name = "cluster-autoscaler"
+  repository = "https://kubernetes.github.io/autoscaler"
+  chart = "cluster-autoscaler"
+  namespace = "kube-system"
+  set = [ {
+    name = "autoDiscovery.clusterName"
+    value = var.cluster_name
+  },
+  {
+    name = "awsRegion"
+    value = var.region
+  },
+  ]
 }
+resource "helm_release" "argocd" {
+  name = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart = "argo-cd"
+  namespace = "argocd"
+  create_namespace = true
+}
+resource "helm_release" "monitoring" {
+  name = "kube-prometheus-stack"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart = "kube-prometheus-stack"
+  namespace = "monitoring"
+  create_namespace = true
+}
+
